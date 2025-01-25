ce8cd94a0ea94a2ab6563f207c04ca552768e4f8
RAMYA09-GS <ramya.gsr09@gmail.com>
2025-01-25 14:58:14 +0530

    push

---
+++ ReverseNumber.java
@@ -0,0 +1,20 @@
+public class ReverseNumber 
+{
+    	public static void main(String[] args) 
+	{
+        
+        	int number = 12345; 
+        	int reversedNumber = 0;
+
+        
+        	for (int temp = number; temp > 0; temp /= 10) 
+		{
+            		int digit = temp % 10; 
+            		reversedNumber = reversedNumber * 10 + digit; 
+        	}
+
+        
+        	System.out.println("Original number: " + number);
+        	System.out.println("Reversed number: " + reversedNumber);
+    	}
+}
